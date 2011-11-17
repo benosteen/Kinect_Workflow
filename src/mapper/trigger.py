2d1d068473a1553220cf91109fa8e9de47735a54
Ben O'Steen <bosteen@gmail.com>
2011-11-17 02:14:25 +0000

    Added usage to readme

---
+++ src/mapper/trigger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+DEFAULT_CONFIG = {'fn':'simple_gt',
+                  'variable':'foo',
+                  'limit':10000}
+
+class Trigger(object):
+    def __init__(self, config = DEFAULT_CONFIG):
+        self.config = config
+        self.set_fn()
+    
+    def set_fn(self, fn=None):
+        if fn != None:
+            if hasattr(self, fn):
+                self.config['fn'] = fn
+            else:
+                print "Couldn't find function '%s' in this class" % fn
+                print "Continuing to use %s" % config.get('fn')
+        if hasattr(self, self.config.get('fn', 'mu_as_min')):
+            self.fn = self.__getattribute__(self.config.get('fn', 'simple_gt'))
+            self.varname = self.config.get("variable")
+    
+    def simple_gt(self, values):
+        # True, False and None for undefined ;)
+        if self.varname in values.keys():
+            return values[self.varname] > self.config['limit']
+
---
+++ src/output/midi.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import pygame, pygame.midi
+
+pygame.midi.init()
+
+DEFAULT_CONFIG = {'fn':'basic_grid', 'device':0, 'grid_width':5}
+
+class Drums(object):
+    def __init__(self, config=DEFAULT_CONFIG):
+        self.config = config
+        self.drumstates = {}
+        self.basenote = 35
+        self.o = pygame.midi.Output(config.get('device', 0))
+        self.set_fn()
+    
+    def set_fn(self, fn=None):
+        if fn != None:
+            if hasattr(self, fn):
+                self.config['fn'] = fn
+            else:
+                print "Couldn't find function '%s' in this class" % fn
+                print "Continuing to use %s" % config.get('fn')
+        if hasattr(self, self.config.get('fn', 'mu_as_min')):
+            self.fn = self.__getattribute__(self.config.get('fn', 'basic_grid'))
+        
+    def basic_grid(self, label, value):
+        x,y = label
+        drums_no = self.basenote+(x + y*self.config.get('grid_width', 5))
+        if drums_no not in self.drumstates.keys():
+            self.drumstates[drums_no] = 0
+
+        if self.drumstates[drums_no] and not value:
+            self.o.note_off(drums_no)
+            self.drumstates[drums_no] = 0
+        if value:
+            if self.drumstates[drums_no] != 1:
+                self.drumstates[drums_no] = 1
+                self.o.note_on(drums_no, 120)
